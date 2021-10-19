ea53ffd3bdcdaae5015fddf8468b1e11b2b86bb9
Michael Vanderpoorten <michael@yungpanda.com>
2021-10-19 13:54:56 +0200

    Events

--- week5/events/index.js
+++ week5/events/index.js
@@ -1,5 +1,39 @@
 (function () {
-  // appendChild
-  // style via classes (show/hide)
-  // events
+  const $body = document.querySelector("body");
+
+  // alternative to innerHTML
+  const $h1 = document.createElement("h1");
+  $h1.innerText = "Heading 1";
+  $body.appendChild($h1);
+
+  const $btn = document.querySelector("#btn");
+  const $color = document.querySelector("#color");
+
+  let clicks = 0;
+
+  $btn.addEventListener(
+    "click",
+    function (event) {
+      console.log($color);
+      clicks++;
+      $color.style.marginLeft = `${clicks * 50}px`;
+    },
+    false
+  );
+
+  $color.addEventListener(
+    "mouseover",
+    function (event) {
+      $color.classList.add("highlight");
+    },
+    false
+  );
+
+  $color.addEventListener(
+    "mouseout",
+    function (event) {
+      $color.classList.remove("highlight");
+    },
+    false
+  );
 })();
